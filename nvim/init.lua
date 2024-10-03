352c37318aacfac8655f95ef27a4ea4ce8333bc5
unknown <marc.baetica@gmail.com>
2024-10-03 16:47:24 -0400

    Py executable

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,6 @@
 require("config.lazy")
 
-vim.keymap.set('n', 'ex1', function() print('Example 1') end)
+vim.keymap.set('n', 'e1', function() print('Example 1.') end)
+vim.keymap.set('n', '<Leader>1', function() print('Pressed Leader 1.') end)
+vim.keymap.set('n', 'ex', ":w <bar> exec '!python3 '.shellscape('%')<CR>")
 
