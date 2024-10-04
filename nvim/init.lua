6683963eabccecefd03c3c9d668cb95333e6e0d1
unknown <marc.baetica@gmail.com>
2024-10-04 18:40:11 -0400

    Events and scripting

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,3 +6,28 @@ vim.keymap.set('n', '<Leader>1', function() print('Pressed Leader 1.') end)
 vim.keymap.set('n', 'ex', ":w <bar> exec '!python3 '.shellscape('%')<CR>")
 vim.keymap.set('n', '<Leader>1', '<bar>python --version<CR>')
 
+-- local os = require('os')
+-- vim.print('This is a test!')
+-- print('This is a test 2!')
+
+-- local desired_path = os.getenv('USERPROFILE') .. '\\Desktop\\Python_projects\\Nvim_config'
+
+-- print(desired_path)
+
+-- local event_name = 'VimEnter'local os = require("os")
+
+local path_to_desktop = os.getenv("USERPROFILE") .. "\\Desktop"
+
+local vim_enter_group = vim.api.nvim_create_augroup("vim_enter_group", { clear = true })
+
+vim.api.nvim_create_autocmd(
+    {"VimEnter"},
+    { pattern = "*", command = "cd " .. path_to_desktop, group = vim_enter_group }
+)
+
+-- vim.api.nvim_create_autocmd(event_name, {pattern={'*'}, callback=print('Works!')})
+
+-- vim.api.nvim_create_autocmd(event_name, {pattern={'*'}, command='cd ' .. desired_path})
+
+
+
--- some_script.lua
+++ some_script.lua
@@ -2,5 +2,15 @@ local os = require('os')
 vim.print('This is a test!')
 print('This is a test 2!')
 
-print(os.getenv('USERPROFILE') .. '\\Desktop\\Python_projects\\Nvim_config')
+local desired_path = os.getenv('USERPROFILE') .. '\\Desktop\\Python_projects\\Nvim_config'
+
+print(desired_path)
+
+local event_name = 'VimEnter'
+
+vim.api.nvim_create_autocmd(event_name, {pattern={'*'}, callback=print('Works!')})
+
+vim.api.nvim_create_autocmd(event_name, {pattern={'*'}, command='cd ' .. desired_path})
+
+
 
