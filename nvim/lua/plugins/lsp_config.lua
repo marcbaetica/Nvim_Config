2447f3ca708fa2df0ec0c2d0f1e2125b0dd3de4a
Marc <marc.baetica@gmail.com>
2024-10-24 22:23:00 -0400

    Added hover option.

--- nvim/lua/plugins/lsp_config.lua
+++ nvim/lua/plugins/lsp_config.lua
@@ -17,6 +17,7 @@ local on_attach = function(_, _)
     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
 
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, {})
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
     --vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_reference, {})     -- Causes error on attach.
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
