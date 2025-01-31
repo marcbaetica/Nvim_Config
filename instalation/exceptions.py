4ec555b4f7022d0ce6afb237cb707f7d525cc388
marcb <marc_baetica@trendmicro.com>
2025-01-30 19:43:47 -0500

    Cleanup.

--- instalation/exceptions.py
+++
@@ -1,8 +0,0 @@
-class NonZeroReturnCodeError(Exception):
-    def __init__(self, code):
-        self.code = code
-        self.msg = f'Shell execution return non-zero code.'
-        super().__init__(self.msg)
-
-    def __str__(self):
-        return f'{self.msg} -> {self.code}'
