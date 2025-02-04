38d71a99e7a753b4dd9b54d8df5c69a3b5500f0f
marcb <marc.baetica@gmail.com>
2025-02-04 13:15:19 -0500

    Fixed non existent mason update command and updated docs.

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -32,7 +32,8 @@ require('lazy').setup({
         { 'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' } },
 
         -- LSP install and config
-        { 'williamboman/mason.nvim', build = ':MasonUpdate' },
+        -- { 'williamboman/mason.nvim', build = ':MasonUpdate' },  -- TODO: find out if :MasonUpdate will trigger automatically
+        { 'williamboman/mason.nvim' },
         { 'williamboman/mason-lspconfig.nvim' },
         { 'neovim/nvim-lspconfig' },
         {
